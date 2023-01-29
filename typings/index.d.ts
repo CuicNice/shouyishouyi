7d244ea6805536e0cbfd582af9f68f54d3b4b1de
CodingGorit <1225277469@qq.com>
2023-01-29 20:43:12 +0800

    code optimization

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -22,3 +22,26 @@ interface IResult<T> {
   msg: string
   data?: T
 }
+
+// 默认 tap 事件传递
+interface ITapEvent<T = any> {
+  currentTarget: { dataset: T; };
+}
+
+// 分页 page 参数
+interface IPage<T = any> {
+  currentPage: number,
+  index: number | undefined,
+  pageSize: number,
+  totalPage?: number,
+  totalCount?: number,
+  list: Array<T>,
+  params?: any,
+  sortColumn?: string,
+  sortMethod?: string
+}
+
+// 通用 string 对象类型
+interface StringReflectI {
+  [key: string]: string
+}
