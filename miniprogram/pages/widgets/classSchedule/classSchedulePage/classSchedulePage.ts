e7551f367625e285795d8fe5cb60b42c7242f8ce
Chentao_Wang <1750568044@qq.com>
2023-02-06 03:05:09 +0800

    课表修改

--- miniprogram/pages/widgets/classSchedule/classSchedulePage/classSchedulePage.ts
+++ miniprogram/pages/widgets/classSchedule/classSchedulePage/classSchedulePage.ts
@@ -20,6 +20,7 @@ Page({
     D:'',
     I:3,
     G:'',
+    shuzu:[],
     classSchedule:{},
     hidden: false,
     Semesterswitchingdetail:false,
@@ -360,7 +361,61 @@ Page({
             }
           }
         }
+        //拿到具体有哪几周有此课，并将其放在数组中
         for(var i=0;i<this.data.classSchedule.data.all_tables.length;i++){
+          var arr=this.data.classSchedule.data.all_tables[i].day_num.split(",")
+          console.log(arr)
+          if(arr.length==2){
+          for(var j=0;j<2;j++){
+            var mycrr=arr[j].match(/\d+(\.\d+)?/g)
+            console.log(mycrr)
+            if(mycrr.length==2){
+            for(var k=mycrr[0];k<=mycrr[1];k++){
+              var leng=k
+              this.setData({shuzu:this.data.shuzu.concat(leng)})
+            }
+          }
+          if(mycrr.length==1){
+            var leng=k
+            this.setData({shuzu:this.data.shuzu.concat(leng)})
+          }
+          }
+          console.log(this.data.shuzu)
+          var abc=this.data.shuzu
+          this.data.classSchedule.data.all_tables[i].abc=abc
+            var kebiao=this.data.classSchedule.data.all_tables
+              this.setData({
+                'classSchedule.data.all_tables':kebiao,
+                shuzu:[]
+              })
+        }
+        if(arr.length==1){
+          var mycrr=arr[0].match(/\d+(\.\d+)?/g)
+          if(mycrr.length==1){
+            var abc=this.data.shuzu.concat(mycrr)
+            this.data.classSchedule.data.all_tables[i].abc=abc
+            var kebiao=this.data.classSchedule.data.all_tables
+              this.setData({
+                'classSchedule.data.all_tables':kebiao,
+                shuzu:[]
+              })
+          }
+          if(mycrr.length==2){
+            for(var y=mycrr[0];y<=mycrr[1];y++){
+              var leng=y
+              this.setData({shuzu:this.data.shuzu.concat(leng)})
+            }
+            var abc=this.data.shuzu
+            this.data.classSchedule.data.all_tables[i].abc=abc
+            var kebiao=this.data.classSchedule.data.all_tables
+              this.setData({
+                'classSchedule.data.all_tables':kebiao,
+                shuzu:[]
+              })
+          }
+        }
+          this.data.classSchedule.data.all_tables[i].shuzu=this.data.shuzu
+          this.setData({shuzu:[]})
           if(this.data.classSchedule.data.all_tables[i].day == '星期一'){
             var daynum=1 as unknown as number
             var start=this.data.classSchedule.data.all_tables[i].num.slice(0,1)
