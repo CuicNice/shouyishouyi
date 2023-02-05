cbb5d4448189067d546ab7f76350d3bd3c18acf0
luosukai <3457823109@qq.com>
2023-02-05 21:03:03 +0800

    静态message
    静态的message

--- miniprogram/components/fmin-topbar/fmin-topbar.ts
+++ miniprogram/components/fmin-topbar/fmin-topbar.ts
@@ -15,15 +15,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    isShow:true,
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    returnEvent() {
+    returnEvent(){
+      this.triggerEvent('sync1');
       wx.navigateBack();
+
     }
   }
 })
