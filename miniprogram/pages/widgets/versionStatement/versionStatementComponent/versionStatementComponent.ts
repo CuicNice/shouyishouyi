28596e80bec579822727afbf6277da4aa56d5ce4
aFang0 <3111659946@qq.com>
2023-02-13 20:41:20 +0800

    页面提交
    版本页面

--- miniprogram/pages/widgets/versionStatement/versionStatementComponent/versionStatementComponent.ts
+++ miniprogram/pages/widgets/versionStatement/versionStatementComponent/versionStatementComponent.ts
@@ -4,6 +4,10 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    boardTitle:{
+      type:String,
+      value:""
+    } 
 
   },
 
--- miniprogram/pages/widgets/versionStatement/versionStatementPage/versionStatementPage.ts
+++ miniprogram/pages/widgets/versionStatement/versionStatementPage/versionStatementPage.ts
@@ -1,17 +1,58 @@
 // pages/widgets/versionStatement/versionStatementPage/versionStatementPage.ts
+// 请求
+// 暂时还没有接口
+// import{getVersionContent,getDeclareContent,getDevelopmentContent}from "../../../../api/versionStarementApi";
+// 构造参数
+// 声明接口定义
+export interface versionStarementItem{
+
+}
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    rectangleIcon:"/static/svg/Rectangle.svg",
 
+  },
+  // 小程序版本详情
+initVersionStatement(){
+  let that= this;
+  //设置标题
+  that.setData({
+    versionBoardTitle:"小程序版本详情"
+  })
+  // 请求渲染数据部分
+  },
+    // 声明详情
+initDeclareStatement(){
+  let that= this;
+  //设置标题
+  that.setData({
+    declareBoardTitle:"小程序声明"
+  })
+  // 请求渲染数据部分
+  },
+    // 开发详情
+initDevStatement(){
+  let that= this;
+  //设置标题
+  that.setData({
+    devBoardTitle:"开发贡献"
+  })
+  // 请求渲染数据部分
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
+  // 返回
+
+
   onLoad() {
+
 
   },
 
@@ -26,6 +67,11 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
+    let that=this;
+    // 初始化标题
+    that.initVersionStatement()
+    that.initDeclareStatement()
+    that.initDevStatement()
 
   },
 
