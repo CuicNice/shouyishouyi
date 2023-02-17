97ab1c4b460e1b75182eedb33a652b66d5987f05
Chentao_Wang <1750568044@qq.com>
2023-02-18 01:55:34 +0800

    课表的移植

--- miniprogram/pages/widgets/classSchedule/classSchedulePage/classSchedulePage.ts
+++ miniprogram/pages/widgets/classSchedule/classSchedulePage/classSchedulePage.ts
@@ -12,6 +12,11 @@ Page({
     startDate:"2023/2/13",
     showClass:false,
     suorec: '',
+    week: [{ index: 1, type: true, day1: '8/28', day2: '8/29', day3: '8/30', day4: '8/31', day5: '9/1', day6: '9/2', day7: '9/3' }, { index: 2, type: true, day1: '9/4', day2: '9/5', day3: '9/6', day4: '9/7', day5: '9/8', day6: '9/9', day7: '9/10' }, { index: 3, type: true, day1: '9/11', day2: '9/12', day3: '9/13', day4: '9/14', day5: '9/15', day6: '9/16', day7: '9/17' }, { index: 4, type: true, day1: '9/18', day2: '9/19', day3: '9/20', day4: '9/21', day5: '9/22', day6: '9/23', day7: '9/24' }, { index: 5, type: true, day1: '9/25', day2: '9/26', day3: '9/27', day4: '9/28', day5: '9/29', day6: '9/30', day7: '10/1' }, { index: 6, type: true, day1: '10/2', day2: '10/3', day3: '10/4', day4: '10/5', day5: '10/6', day6: '10/7', day7: '10/8' }, { index: 7, type: true, day1: '10/9', day2: '10/10', day3: '10/11', day4: '10/12', day5: '10/13', day6: '10/14', day7: '10/15' }, { index: 8, type: true, day1: '10/16', day2: '10/17', day3: '10/18', day4: '10/19', day5: '10/20', day6: '10/21', day7: '10/22' }, { index: 9, type: true, day1: '10/23', day2: '10/24', day3: '10/25', day4: '10/26', day5: '10/27', day6: '10/28', day7: '10/29' }, { index: 10, type: true, day1: '10/30', day2: '10/31', day3: '11/1', day4: '11/2', day5: '11/3', day6: '11/4', day7: '11/5' }, { index: 11, type: true, day1: '11/6', day2: '11/7', day3: '11/8', day4: '11/9', day5: '11/10', day6: '11/11', day7: '11/12' }, { index: 12, type: true, day1: '11/13', day2: '11/14', day3: '11/15', day4: '11/16', day5: '11/17', day6: '11/18', day7: '11/19' }, { index: 13, type: true, day1: '11/20', day2: '11/21', day3: '11/22', day4: '11/23', day5: '11/24', day6: '11/25', day7: '11/26' }, { index: 14, type: true, day1: '11/27', day2: '11/28', day3: '11/29', day4: '11/30', day5: '12/1', day6: '12/2', day7: '12/3' }, { index: 15, type: true, day1: '12/4', day2: '12/5', day3: '12/6', day4: '12/7', day5: '12/8', day6: '12/9', day7: '12/10' }, { index: 16, type: true, day1: '12/11', day2: '12/12', day3: '12/13', day4: '12/14', day5: '12/15', day6: '12/16', day7: '12/17' }, { index: 17, type: true, day1: '12/18', day2: '12/19', day3: '12/20', day4: '12/21', day5: '12/22', day6: '12/23', day7: '12/24' }, { index: 18, type: true, day1: '12/25', day2: '12/26', day3: '12/27', day4: '12/28', day5: '12/29', day6: '12/30', day7: '12/31' }, { index: 19, type: true, day1: '1/1', day2: '1/2', day3: '1/3', day4: '1/4', day5: '1/5', day6: '1/6', day7: '1/7' }],
+    M:'',
+    Y:'',
+    D:'',
+    I:3,
     Semesterswitchingdetail:false,
     weeksStatic: [{
         title: "周日",
@@ -53,13 +58,13 @@ Page({
   },
 
   bindEletricCharge(){
-    if (this.data.suorec == '大一下' || this.data.suorec == '大二下' || this.data.suorec == '大三下' || this.data.suorec == '大四下') {
+    if (this.data.suorec == '大一下学期' || this.data.suorec == '大二下学期' || this.data.suorec == '大三下学期' || this.data.suorec == '大四下学期') {
       this.setData({
         I: 12,
-        week: [{ index: 1, type: true, day1: '2/19', day2: '2/20', day3: '2/21', day4: '2/22', day5: '2/23', day6: '2/24', day7: '2/25' }, { index: 2, type: true, day1: '2/26', day2: '2/27', day3: '2/28', day4: '3/1', day5: '3/2', day6: '3/3', day7: '3/4' }, { index: 3, type: true, day1: '3/5', day2: '3/6', day3: '3/7', day4: '3/8', day5: '3/9', day6: '3/10', day7: '3/11' }, { index: 4, type: true, day1: '3/12', day2: '3/13', day3: '3/14', day4: '3/15', day5: '3/16', day6: '3/17', day7: '3/18' }, { index: 5, type: true, day1: '3/19', day2: '3/20', day3: '3/21', day4: '3/22', day5: '3/23', day6: '3/24', day7: '3/25' }, { index: 6, type: true, day1: '3/26', day2: '3/27', day3: '3/28', day4: '3/29', day5: '3/30', day6: '3/31', day7: '4/1' }, { index: 7, type: true, day1: '4/2', day2: '4/3', day3: '4/4', day4: '4/5', day5: '4/6', day6: '4/7', day7: '4/8' }, { index: 8, type: true, day1: '4/9', day2: '4/10', day3: '4/11', day4: '4/12', day5: '4/13', day6: '4/14', day7: '4/15' }, { index: 9, type: true, day1: '4/16', day2: '4/17', day3: '4/18', day4: '4/19', day5: '4/20', day6: '4/21', day7: '4/22' }, { index: 10, type: true, day1: '4/23', day2: '4/24', day3: '4/25', day4: '4/26', day5: '4/27', day6: '4/28', day7: '4/29' }, { index: 11, type: true, day1: '4/30', day2: '5/1', day3: '5/2', day4: '5/3', day5: '5/4', day6: '5/5', day7: '5/6' }, { index: 12, type: true, day1: '5/7', day2: '5/8', day3: '5/9', day4: '5/10', day5: '5/11', day6: '5/12', day7: '5/13' }, { index: 13, type: true, day1: '5/14', day2: '5/15', day3: '5/16', day4: '5/17', day5: '5/18', day6: '5/19', day7: '5/20' }, { index: 14, type: true, day1: '5/21', day2: '5/22', day3: '5/23', day4: '5/24', day5: '5/25', day6: '5/26', day7: '5/27' }, { index: 15, type: true, day1: '5/28', day2: '5/29', day3: '5/30', day4: '5/31', day5: '6/1', day6: '6/2', day7: '6/3' }, { index: 16, type: true, day1: '6/4', day2: '6/5', day3: '6/6', day4: '6/7', day5: '6/8', day6: '6/9', day7: '6/10' }, { index: 17, type: true, day1: '6/11', day2: '6/12', day3: '6/13', day4: '6/14', day5: '6/15', day6: '6/16', day7: '6/17' }, { index: 18, type: true, day1: '6/18', day2: '6/19', day3: '6/20', day4: '6/21', day5: '6/22', day6: '6/23', day7: '6/24' }, { index: 19, type: true, day1: '6/25', day2: '6/26', day3: '6/27', day4: '6/28', day5: '6/29', day6: '6/30', day7: '7/1' }]
+       week: [{ index: 1, type: true, day1: '2/19', day2: '2/20', day3: '2/21', day4: '2/22', day5: '2/23', day6: '2/24', day7: '2/25' }, { index: 2, type: true, day1: '2/26', day2: '2/27', day3: '2/28', day4: '3/1', day5: '3/2', day6: '3/3', day7: '3/4' }, { index: 3, type: true, day1: '3/5', day2: '3/6', day3: '3/7', day4: '3/8', day5: '3/9', day6: '3/10', day7: '3/11' }, { index: 4, type: true, day1: '3/12', day2: '3/13', day3: '3/14', day4: '3/15', day5: '3/16', day6: '3/17', day7: '3/18' }, { index: 5, type: true, day1: '3/19', day2: '3/20', day3: '3/21', day4: '3/22', day5: '3/23', day6: '3/24', day7: '3/25' }, { index: 6, type: true, day1: '3/26', day2: '3/27', day3: '3/28', day4: '3/29', day5: '3/30', day6: '3/31', day7: '4/1' }, { index: 7, type: true, day1: '4/2', day2: '4/3', day3: '4/4', day4: '4/5', day5: '4/6', day6: '4/7', day7: '4/8' }, { index: 8, type: true, day1: '4/9', day2: '4/10', day3: '4/11', day4: '4/12', day5: '4/13', day6: '4/14', day7: '4/15' }, { index: 9, type: true, day1: '4/16', day2: '4/17', day3: '4/18', day4: '4/19', day5: '4/20', day6: '4/21', day7: '4/22' }, { index: 10, type: true, day1: '4/23', day2: '4/24', day3: '4/25', day4: '4/26', day5: '4/27', day6: '4/28', day7: '4/29' }, { index: 11, type: true, day1: '4/30', day2: '5/1', day3: '5/2', day4: '5/3', day5: '5/4', day6: '5/5', day7: '5/6' }, { index: 12, type: true, day1: '5/7', day2: '5/8', day3: '5/9', day4: '5/10', day5: '5/11', day6: '5/12', day7: '5/13' }, { index: 13, type: true, day1: '5/14', day2: '5/15', day3: '5/16', day4: '5/17', day5: '5/18', day6: '5/19', day7: '5/20' }, { index: 14, type: true, day1: '5/21', day2: '5/22', day3: '5/23', day4: '5/24', day5: '5/25', day6: '5/26', day7: '5/27' }, { index: 15, type: true, day1: '5/28', day2: '5/29', day3: '5/30', day4: '5/31', day5: '6/1', day6: '6/2', day7: '6/3' }, { index: 16, type: true, day1: '6/4', day2: '6/5', day3: '6/6', day4: '6/7', day5: '6/8', day6: '6/9', day7: '6/10' }, { index: 17, type: true, day1: '6/11', day2: '6/12', day3: '6/13', day4: '6/14', day5: '6/15', day6: '6/16', day7: '6/17' }, { index: 18, type: true, day1: '6/18', day2: '6/19', day3: '6/20', day4: '6/21', day5: '6/22', day6: '6/23', day7: '6/24' }, { index: 19, type: true, day1: '6/25', day2: '6/26', day3: '6/27', day4: '6/28', day5: '6/29', day6: '6/30', day7: '7/1' }]
       })
     }
-    if (this.data.suorec == '大一上' || this.data.suorec == '大二上' || this.data.suorec == '大三上' || this.data.suorec == '大四上') {
+    if (this.data.suorec == '大一上学期' || this.data.suorec == '大二上学期' || this.data.suorec == '大三上学期' || this.data.suorec == '大四上学期') {
       this.setData({
         I: 3,
         week: [{ index: 1, type: true, day1: '8/28', day2: '8/29', day3: '8/30', day4: '8/31', day5: '9/1', day6: '9/2', day7: '9/3' }, { index: 2, type: true, day1: '9/4', day2: '9/5', day3: '9/6', day4: '9/7', day5: '9/8', day6: '9/9', day7: '9/10' }, { index: 3, type: true, day1: '9/11', day2: '9/12', day3: '9/13', day4: '9/14', day5: '9/15', day6: '9/16', day7: '9/17' }, { index: 4, type: true, day1: '9/18', day2: '9/19', day3: '9/20', day4: '9/21', day5: '9/22', day6: '9/23', day7: '9/24' }, { index: 5, type: true, day1: '9/25', day2: '9/26', day3: '9/27', day4: '9/28', day5: '9/29', day6: '9/30', day7: '10/1' }, { index: 6, type: true, day1: '10/2', day2: '10/3', day3: '10/4', day4: '10/5', day5: '10/6', day6: '10/7', day7: '10/8' }, { index: 7, type: true, day1: '10/9', day2: '10/10', day3: '10/11', day4: '10/12', day5: '10/13', day6: '10/14', day7: '10/15' }, { index: 8, type: true, day1: '10/16', day2: '10/17', day3: '10/18', day4: '10/19', day5: '10/20', day6: '10/21', day7: '10/22' }, { index: 9, type: true, day1: '10/23', day2: '10/24', day3: '10/25', day4: '10/26', day5: '10/27', day6: '10/28', day7: '10/29' }, { index: 10, type: true, day1: '10/30', day2: '10/31', day3: '11/1', day4: '11/2', day5: '11/3', day6: '11/4', day7: '11/5' }, { index: 11, type: true, day1: '11/6', day2: '11/7', day3: '11/8', day4: '11/9', day5: '11/10', day6: '11/11', day7: '11/12' }, { index: 12, type: true, day1: '11/13', day2: '11/14', day3: '11/15', day4: '11/16', day5: '11/17', day6: '11/18', day7: '11/19' }, { index: 13, type: true, day1: '11/20', day2: '11/21', day3: '11/22', day4: '11/23', day5: '11/24', day6: '11/25', day7: '11/26' }, { index: 14, type: true, day1: '11/27', day2: '11/28', day3: '11/29', day4: '11/30', day5: '12/1', day6: '12/2', day7: '12/3' }, { index: 15, type: true, day1: '12/4', day2: '12/5', day3: '12/6', day4: '12/7', day5: '12/8', day6: '12/9', day7: '12/10' }, { index: 16, type: true, day1: '12/11', day2: '12/12', day3: '12/13', day4: '12/14', day5: '12/15', day6: '12/16', day7: '12/17' }, { index: 17, type: true, day1: '12/18', day2: '12/19', day3: '12/20', day4: '12/21', day5: '12/22', day6: '12/23', day7: '12/24' }, { index: 18, type: true, day1: '12/25', day2: '12/26', day3: '12/27', day4: '12/28', day5: '12/29', day6: '12/30', day7: '12/31' }, { index: 19, type: true, day1: '1/1', day2: '1/2', day3: '1/3', day4: '1/4', day5: '1/5', day6: '1/6', day7: '1/7' }]
@@ -69,6 +74,10 @@ Page({
     if (this.data.suorec.slice(1, 2) == "二") { this.setData({ Y: parseInt(wx.getStorageSync('key1').slice(0, 4)) + 1 as unknown as string }) }
     if (this.data.suorec.slice(1, 2) == "三") { this.setData({ Y: parseInt(wx.getStorageSync('key1').slice(0, 4)) + 2 as unknown as string }) }
     if (this.data.suorec.slice(1, 2) == "四") { this.setData({ Y: parseInt(wx.getStorageSync('key1').slice(0, 4)) + 3 as unknown as string }) }
+    this.setData({semester:this.data.suorec.slice(0, 3)})
+    this.setData({Semesterswitchingdetail:false})
+    wx.removeStorageSync('classSchedule')
+    this.initClassData()
   },
 
   /**
@@ -82,8 +91,8 @@ Page({
         url: 'http://www.fmin-courses.com:9527/api/v1/craw/user/classTable',
         method: 'POST',
         data: {
-          "zh": "20211107097",
-          "mm": "Hushnow0729",
+          "zh": wx.getStorageSync('key1'),
+          "mm": wx.getStorageSync('key2'),
           "year": year,
           "num": num
         },
@@ -170,7 +179,7 @@ Page({
       wx.showLoading({
         title: '加载中。。。',
       })
-      var res = await this.getTableDataFromApi(2022, 3);
+      var res = await this.getTableDataFromApi(parseInt(this.data.Y), this.data.I);
       console.log(res)
       if (res.data.code == 20000) {
         var all_tables = res.data.data.all_tables;
@@ -431,7 +440,6 @@ Page({
    var that = this;
     var index = e.currentTarget.dataset.index + 1;
     var nowWeekData = that.getNowWeekData(this.data.classSchedule,index);
-
     var nowDate = new Date(this.data.startDate) //获取指定日期当周的一周日期
     var date = new Date(nowDate.getTime() + 24 * 60 * 60 * 1000 * (index - 1)* 7);
     this.getWeekTime(date);
@@ -445,6 +453,45 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
+     /**
+     * 获取当前年月
+     */
+    var timestamp = Date.parse(new Date() as unknown as string);
+    var date = new Date(timestamp);
+    this.setData({
+      //获取年份
+      Y:2022/* date.getFullYear() */ as unknown as string,
+      //获取月份
+      M:8/*  (date.getMonth() + 1 < 10 ? (date.getMonth() + 1) : date.getMonth() + 1)  */as unknown as string,
+      //获取当日日期
+      D: 28/* date.getDate() < 10 ?(date.getDate())as unknown as string : date.getDate() */ as unknown as string
+    })
+    if (8 <= parseInt(this.data.M) && parseInt(this.data.M) <= 12) {
+      console.log(this.data.M)
+      this.setData({ Y: this.data.Y + 1 , I: 3 })
+    }
+    if (1 <= parseInt(this.data.M) && parseInt(this.data.M) < 2) {
+      console.log(this.data.M)
+      this.setData({ I: 3 })
+    }
+    if (2 <= parseInt(this.data.M) && parseInt(this.data.M) < 8) {
+      console.log(this.data.M)
+      this.setData({ I: 12 })
+    }
+    console.log(this.data.M + '/' + this.data.D)
+    if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 4 && 8 <= parseInt(this.data.M) && parseInt(this.data.M) <= 12) { this.setData({ semester: "大四上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 4 && 1 <= parseInt(this.data.M) && parseInt(this.data.M) < 2) { this.setData({ semester: "大四上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 4 && 2 <= parseInt(this.data.M) && parseInt(this.data.M) < 8) { this.setData({ semester: "大四下" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 3 && 8 <= parseInt(this.data.M) && parseInt(this.data.M) <= 12) { this.setData({ semester: "大三上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 3 && 1 <= parseInt(this.data.M) && parseInt(this.data.M) < 2) { this.setData({ semester: "大三上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 3 && 2 <= parseInt(this.data.M) && parseInt(this.data.M) < 8) { this.setData({ semester: "大三下" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 2 && 8 <= parseInt(this.data.M) && parseInt(this.data.M) <= 12) { this.setData({ semester: "大二上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 2 && 1 <= parseInt(this.data.M) && parseInt(this.data.M) < 2) { this.setData({ semester: "大二上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 2 && 2 <= parseInt(this.data.M) && parseInt(this.data.M) < 8) { this.setData({ semester: "大二下" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 1 && 8 <= parseInt(this.data.M) && parseInt(this.data.M) <= 12) { this.setData({ semester: "大一上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 1 && 1 <= parseInt(this.data.M) && parseInt(this.data.M) < 2) { this.setData({ semester: "大一上" }) }
+      if (this.data.Y as unknown as number - wx.getStorageSync('key1').slice(0, 4) == 1 && 2 <= parseInt(this.data.M) && parseInt(this.data.M) < 8) { this.setData({ semester: "大一下" }) }
+      this.setData({Y:(parseInt(this.data.Y)-1)as unknown as string})
     this.initPageData();
   },
 
