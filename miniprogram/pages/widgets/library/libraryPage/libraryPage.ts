8517f38ad031e29ea26f0acf001468a59cb3a747
Chentao_Wang <1750568044@qq.com>
2023-02-19 15:49:29 +0800

    图书馆分页修改完成

--- miniprogram/pages/widgets/library/libraryPage/libraryPage.ts
+++ miniprogram/pages/widgets/library/libraryPage/libraryPage.ts
@@ -18,13 +18,11 @@ Page({
     ifshow: true,
     swiper: 0,  //当前所在页面的 
     shuju: [] as any,
-    ifsearch: true,
     allbook: [] as any,
     a: 1,
     b: 0,
-    ifshowXiang: true,
     num: 0,
-    electricChargeTitle:'首义图书馆'
+    electricChargeTitle:'首义图书馆',
   },
 
   swiperChangeqian: function () {
@@ -38,15 +36,6 @@ Page({
         swiper: this.data.swiper + 1
       })
     }
-  },
-
-  returnEvent() {
-    this.setData({ ifsearch: true,})
-    
-  },
-
-  return() {
-    this.setData({ ifshowXiang: true })
   },
 
   getInputValue: function (e: any) {
@@ -71,203 +60,12 @@ Page({
   markMake(e: any) {
     var num = wx.getStorageSync('item').length - e.currentTarget.dataset.index - 1
     var add = wx.getStorageSync('item')[num].item
-    this.setData({ word: add, ifsearch: false })
+    this.setData({ word: add})
     setTimeout(() => {
-      this.webrequest(this.data.a)
+      wx.navigateTo({url:'/pages/widgets/library/librarytext/librarytext?word='+this.data.word}),
+      this.setData({word:''})
+    this.get()
     }, 500)
-  },
-
-  webrequest(a: number) {
-    this.selectComponent("#toast").showToast("请求中....", "lodding");
-    wx.request({
-      url: 'http://www.fmin-courses.com:9527/api/v1/craw/library/searchBook',
-      method: "POST",
-      data: {
-        "page": a,
-        "word": this.data.word
-      },
-      success: (res: any) => {
-        var arr = res.data.data
-        //console.log(arr)
-        try {
-          if (arr.length != 0) {
-            for (var j = 0; j < 10; j++) {
-              var num = {}
-              arr[j].num = num
-              var jianum = 0
-              var zongnum = 0
-              var zhongnum = 0
-              var nannum = 0
-              var jiaArr = {}
-              arr[j].num.jiaArr = jiaArr
-              var myarr: never[] = []
-              arr[j].num.jiaArr.myarr = myarr
-              var zongArr = {}
-              arr[j].num.zongArr = zongArr
-              var myarr: never[] = []
-              arr[j].num.zongArr.myarr = myarr
-              var nanArr = {}
-              arr[j].num.nanArr = nanArr
-              var myarr: never[] = []
-              arr[j].num.nanArr.myarr = myarr
-              var zhongArr = {}
-              arr[j].num.zhongArr = zhongArr
-              var myarr: never[] = []
-              arr[j].num.zhongArr.myarr = myarr
-              var add = 0
-              for (var i = 0; i < arr[j].books.length; i++) {
-                if (arr[j].books[i].local.indexOf("嘉鱼") != -1) {
-                  let dataAll = arr[j].num.jiaArr.myarr
-                  add = add + parseInt(arr[j].books[i].hldallnum)
-                  var item = { leixing: "外借图书", num: "A0427181", zhuangtai: "入藏" }
-                  item.leixing = arr[j].books[i].barcode
-                  item.num = arr[j].books[i].localstatu
-                  item.zhuangtai = arr[j].books[i].cirType
-                  dataAll.push(item)
-                  jianum = jianum + 1
-                  var showjia = true
-                  var place = "嘉鱼"
-                  var image = "../../../../static/svg/jiayulibrary.svg"
-                  var hao = arr[j].books[i].callno
-                  arr[j].showjia = showjia
-                  arr[j].num.jiaArr.num = jianum
-                  arr[j].num.jiaArr.hao = hao
-                  arr[j].num.jiaArr.place = place
-                  arr[j].num.jiaArr.image = image
-                }
-                if (arr[j].books[i].local.indexOf("总馆") != -1) {
-                  let dataAll = arr[j].num.zongArr.myarr
-                  add = add + parseInt(arr[j].books[i].hldallnum)
-                  var item = { leixing: "外借图书", num: "A0427181", zhuangtai: "入藏" }
-                  item.leixing = arr[j].books[i].barcode
-                  item.num = arr[j].books[i].localstatu
-                  item.zhuangtai = arr[j].books[i].cirType
-                  dataAll.push(item)
-                  zongnum = zongnum + 1
-                  var showzong = true
-                  var place = "总馆"
-                  var image = "../../../../static/svg/genlibrary.svg"
-                  var hao = arr[j].books[i].callno
-                  arr[j].showzong = showzong
-                  arr[j].num.zongArr.num = zongnum
-                  arr[j].num.zongArr.hao = hao
-                  arr[j].num.zongArr.place = place
-                  arr[j].num.zongArr.image = image
-                }
-                if (arr[j].books[i].local.indexOf("南区") != -1) {
-                  let dataAll = arr[j].num.nanArr.myarr
-                  add = add + parseInt(arr[j].books[i].hldallnum)
-                  var item = { leixing: "外借图书", num: "A0427181", zhuangtai: "入藏" }
-                  item.leixing = arr[j].books[i].barcode
-                  item.num = arr[j].books[i].localstatu
-                  item.zhuangtai = arr[j].books[i].cirType
-                  dataAll.push(item)
-                  nannum = nannum + 1
-                  var shownan = true
-                  var place = "南区"
-                  var image = "../../../../static/svg/nanlibrary.svg"
-                  var hao = arr[j].books[i].callno
-                  arr[j].shownan = shownan
-                  arr[j].num.nanArr.num = nannum
-                  arr[j].num.nanArr.hao = hao
-                  arr[j].num.nanArr.place = place
-                  arr[j].num.nanArr.image = image
-                }
-                if (arr[j].books[i].local.indexOf("中区") != -1 || arr[j].books[i].local.indexOf("南湖") != -1) {
-                  let dataAll = arr[j].num.zhongArr.myarr
-                  add = add + parseInt(arr[j].books[i].hldallnum)
-                  var item = { leixing: "外借图书", num: "A0427181", zhuangtai: "入藏" }
-                  item.leixing = arr[j].books[i].barcode
-                  item.num = arr[j].books[i].localstatu
-                  item.zhuangtai = arr[j].books[i].cirType
-                  dataAll.push(item)
-                  zhongnum = zhongnum + 1
-                  var showzhong = true
-                  var place = "中区"
-                  var image = "../../../../static/svg/schoolBuilt/zhongqutushuguan.svg"
-                  var hao = arr[j].books[i].callno
-                  arr[j].showzhong = showzhong
-                  arr[j].num.zhongArr.num = zhongnum
-                  arr[j].num.zhongArr.hao = hao
-                  arr[j].num.zhongArr.place = place
-                  arr[j].num.zhongArr.image = image
-                }
-              }
-              try {
-                arr[j].add = add
-              } catch { }//此处是为了防止第一个数据中为空产生报错
-            }
-            this.setData({ allbook: arr })
-            for (var i = 0; i < 10; i++) {
-              if (this.data.allbook[i].num.jiaArr.myarr.length == 0) {
-                delete this.data.allbook[i].num.jiaArr
-              }
-              if (this.data.allbook[i].num.nanArr.myarr.length == 0) {
-                delete this.data.allbook[i].num.nanArr
-              }
-              if (this.data.allbook[i].num.zhongArr.myarr.length == 0) {
-                delete this.data.allbook[i].num.zhongArr
-              }
-              if (this.data.allbook[i].num.zongArr.myarr.length == 0) {
-                delete this.data.allbook[i].num.zongArr
-              }
-            }
-            this.setData({ allbook: this.data.allbook })
-            var array = wx.getStorageSync('book') || []
-            //console.log(this.data.allbook)
-            array.push({
-              item: this.data.allbook,
-            })
-            //console.log(this.data.allbook)
-            wx.setStorageSync('book', array)
-          }
-        } catch { }
-        this.selectComponent("#toast").showToastAuto("请求成功", "success")
-      }
-    })
-  },
-
-  againrequest() {
-    try {
-      if (this.data.a == this.data.b + 1 && wx.getStorageSync('book')[this.data.b].item.length != 0) {
-        this.setData({ a: this.data.a + 1, b: this.data.b + 1 })
-        this.webrequest(this.data.a)
-      }
-      if (this.data.a != this.data.b + 1) {
-        this.setData({ b: this.data.b + 1 })
-        this.setData({ allbook: wx.getStorageSync('book')[this.data.b].item })
-      }
-    } catch { }
-  },
-
-  returned(){
-    if(this.data.electricChargeTitle=="首义图书馆"){
-      this.setData({ifshowXiang:true,ifsearch:true, word: '', a: 1, b: 0, allbook: [] }),
-      wx.removeStorage({
-        key: 'book',
-      })
-      this.get()
-    }
-    if(this.data.electricChargeTitle=="图书详情"){
-      this.setData({ifshowXiang:true,electricChargeTitle:"首义图书馆"})
-    }
-  },
-
-  showXQ(res: any) {
-    this.setData({
-      ifshowXiang: false,
-      electricChargeTitle:"图书详情"
-    })
-    console.log(res.currentTarget.dataset.index)
-    this.setData({ num: res.currentTarget.dataset.index })
-  },
-
-  leftrequest() {
-    console.log(wx.getStorageSync('book')[this.data.b])
-    if (this.data.b != 0) {
-      this.setData({ b: this.data.b - 1 })
-      this.setData({ allbook: wx.getStorageSync('book')[this.data.b].item })
-    }
   },
 
   deleteMark() {
@@ -284,13 +82,21 @@ Page({
     })
   },
 
+//查重
+objHeavy: function (arr: { [x: string]: any }) {
+  let arr1 = [];
+  let newArr = [];
+  for (let i in arr) {
+    if (arr1.indexOf(arr[i].item) == -1) {
+      arr1.push(arr[i].item);
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+},
+
   search() {
-    this.setData({ a: 1, b: 0, allbook: [] })
-    wx.removeStorage({
-      key: 'book',
-    })
     if (this.data.word != "") {
-      this.webrequest(this.data.a)
       if (wx.getStorageSync('item').length < 10) {
         this.cache('item')
         var arr = this.objHeavy(wx.getStorageSync('item'))
@@ -320,70 +126,11 @@ Page({
           // 重新设置缓存
           wx.setStorageSync('item', arrays)
         }
-      }
-      this.setData({
-        ifsearch: false
-      })
-    }
+      }}
+      this.get()
+    wx.navigateTo({url:'/pages/widgets/library/librarytext/librarytext?word='+this.data.word})
+    this.setData({word:''})
   },
-
-  /*login() {
-    wx.request({
-      url: 'http://www.fmin-courses.com:9527/api/v1/craw/library/login',
-      method:"POST",
-      data:{
-        "userId": this.data.xuehao,
-        "password": this.data.mima
-      },
-      success:(res)=>{
-        console.log(res)
-        if(res.data.code == 20000){
-          this.selectComponent("#toast").showToastAuto("登录成功", "success");
-          setTimeout(()=>{
-            wx.navigateTo({
-              url: '/pages/Widgets/libraryCollection/libraryCollection?userId=' + this.data.xuehao + '&password=' + this.data.mima,
-            })
-          }, 1000)
-        }
-        if(res.data.code == 30000){
-          console.log('ddd')
-          this.setData({
-            haveBind:false
-          })
-        }
-      }
-    })
-    try {
-      wx.setStorageSync('key1',this.data.xuehao)
-    } catch (e) {  
-    }
-    try {
-      wx.setStorageSync('key2', this.data.mima)
-    } catch (e) {  
-    }
-  },
-
-  cancel(){
-    wx.setClipboardData({
-      data: 'http://ilas.lib.wsyu.edu.cn/index.aspx',
-      success:()=> {
-        wx.hideLoading()
-        this.selectComponent("#toast").showToastAuto("复制链接成功", "success");
-     }
-    })
-  },
-
-  input1(res: { detail: { value: any } }){
-    this.setData({
-      xuehao:res.detail.value
-    })
-  },
-
-  input2(res: { detail: { value: any } }){
-    this.setData({
-      mima:res.detail.value
-    })
-  },*/
 
   swiperChangeho: function () {
     if (this.data.swiper == 0) {
@@ -415,18 +162,6 @@ Page({
       }
   },
 
-  //查重
-  objHeavy: function (arr: { [x: string]: any }) {
-    let arr1 = [];
-    let newArr = [];
-    for (let i in arr) {
-      if (arr1.indexOf(arr[i].item) == -1) {
-        arr1.push(arr[i].item);
-        newArr.push(arr[i]);
-      }
-    }
-    return newArr;
-  },
 
   /**
    * 生命周期函数--监听页面加载
