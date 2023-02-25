ba5b17795cabf74d3e161ae997ae62f6c6da1591
aFang0 <3111659946@qq.com>
2023-02-25 20:09:04 +0800

    新闻部分，正在做组件整改

--- miniprogram/api/newsApi.ts
+++ miniprogram/api/newsApi.ts
@@ -11,6 +11,8 @@ import HttpUtils from "../utils/request";
 import Utils from '../utils/util';
 // 分页的请求参数
 import {outnewsListItem,innernewsListItem} from "../pages/news/list/list";
+import {newsKeyWordsItem} from "../pages/news/search/search";
+
 const request = HttpUtils.getInstance();
 const baseUrl = Utils.getBaseUrl();
 const apiList = {
@@ -25,6 +27,10 @@ const apiList = {
   // 根据ID获取新闻
   // http://www.fmin-courses.com:9527/api/v1/sy/mini/innews/getInNewsById
   getDevDesItemUrl: baseUrl + '/api/v1/sy/mini/innews/getInNewsById',
+  // 模糊查询
+  // http://www.fmin-courses.com:9527/api/v1/sy/mini/innews/getNewsByTitle
+  getNewsByKeyWordsUrl:baseUrl+'/api/v1/sy/mini/innews/getNewsByTitle'
+
 }
 // http://www.fmin-courses.com:9527
 // 内网新闻list
@@ -41,5 +47,13 @@ export const getOutNewsListitem = async (outnewsListItem:any) => {
     return Result.mockSuccess("<h1> 倒计时查询成功 </h1>");
   } else {
     return await request.post(apiList.getOutNewsListitemUrl,outnewsListItem);
+  }
+}
+// 模糊查询,只需要输入1关键字
+export const getNewsByKeyWords = async (newsKeyWordsItem:any) => {
+  if (Consts.DEBUG) {
+    return Result.mockSuccess("<h1> 倒计时查询成功 </h1>");
+  } else {
+    return await request.post(apiList.getNewsByKeyWordsUrl,newsKeyWordsItem);
   }
 }
