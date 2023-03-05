e328e1b25295a293b7dffdc1a7402a3a2371a23c
aFang0 <3111659946@qq.com>
2023-03-06 02:25:56 +0800

    修改url

---
+++ miniprogram/app.ts
@@ -0,0 +1,15 @@
+// app.ts
+const {
+  myStorage
+} = require("./utils/newStorage.js")
+
+App<IAppOption>({
+  globalData: {
+    baseUrl: "https://www.fmin-courses.com"
+  },
+  onLaunch() {
+    // 虎哥
+     // 检查小程序更新
+   
+  },
+})
