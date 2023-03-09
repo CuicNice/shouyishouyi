3dab9279161b51f02aaf7255c144309812058ac1
aFang0 <3111659946@qq.com>
2023-03-10 00:17:22 +0800

    删除动画

--- miniprogram/pages/myWidgets/settingWidgets/settingWidgets.ts
+++ miniprogram/pages/myWidgets/settingWidgets/settingWidgets.ts
@@ -45,7 +45,7 @@ Page({
     /**
      * 选择小组件页面
      */
-    isChooseSeetingwidget:false as Boolean,
+    isChooseSeetingwidget: false as Boolean,
     windowHeight: 0 as number,//屏幕高度
     settingWidgetTitle: "设置小组件",
     hiddenRemindBox: false, // 隐藏remindBox
@@ -56,184 +56,184 @@ Page({
      */
     WedgetsLongPressMoveData: {
       tabList: [{
-          name: '十步杀一人',
-          width: 30,
-          wedgetSvgUrl:'/static/svg/myWidget/weekSchedule.png'
-        },
-        {
-          name: '十步杀一人',
-          width: 30,
-          wedgetSvgUrl:'/static/svg/myWidget/weekSchedule.png'
-        },
-        {
-          name: '十步杀一人',
-          width: 30,
-          wedgetSvgUrl:'/static/svg/myWidget/weekSchedule.png'
-        },
-        {
-          name: '十步杀一人',
-          width: 30,
-          wedgetSvgUrl:'/static/svg/myWidget/weekSchedule.png'
-        },
-        {
-          name: '十步杀一人',
-          width: 30,
-          wedgetSvgUrl:'/static/svg/myWidget/weekSchedule.png'
-        },
-        {
-          name: '十步杀一人',
-          width: 30,
-          wedgetSvgUrl:'/static/svg/myWidget/weekSchedule.png'
-        },
-        {
-          name: '十步杀一人',
-          width: 30,
-          wedgetSvgUrl:'/static/svg/myWidget/weekSchedule.png'
-        }
+        name: '十步杀一人',
+        width: 30,
+        wedgetSvgUrl: '/static/svg/myWidget/weekSchedule.png'
+      },
+      {
+        name: '十步杀一人',
+        width: 30,
+        wedgetSvgUrl: '/static/svg/myWidget/weekSchedule.png'
+      },
+      {
+        name: '十步杀一人',
+        width: 30,
+        wedgetSvgUrl: '/static/svg/myWidget/weekSchedule.png'
+      },
+      {
+        name: '十步杀一人',
+        width: 30,
+        wedgetSvgUrl: '/static/svg/myWidget/weekSchedule.png'
+      },
+      {
+        name: '十步杀一人',
+        width: 30,
+        wedgetSvgUrl: '/static/svg/myWidget/weekSchedule.png'
+      },
+      {
+        name: '十步杀一人',
+        width: 30,
+        wedgetSvgUrl: '/static/svg/myWidget/weekSchedule.png'
+      },
+      {
+        name: '十步杀一人',
+        width: 30,
+        wedgetSvgUrl: '/static/svg/myWidget/weekSchedule.png'
+      }
       ],
       //移动的是哪个元素块
       moveId: null,
       lastY: null,
-      enable:false
+      enable: false
     },
   },
-/**
- * 组件选中
- */
-initSortData(tabList:any) {
-  var height = 0;
-  var ls = [];
-  for(var i=0;i<tabList.length;i++){
-    var obj = tabList[i];
-    obj.y = height;
-    ls.push(obj);
-    height = height + obj.width + 20;
-  }
-  // console.log(ls)
-  this.setData({
-    tabList: ls,
-    viewHeight: height
-  })
-},
-//计算位置
-moveStatus(e:any) {
-  
-  if (e.type == 'touchmove' && this.data.WedgetsLongPressMoveData.enable) {
-    //最终坐标
-    // console.log(e)
-    var y = e.changedTouches[0].pageY;
-    var clientY = e.changedTouches[0].clientY;
-    var nowItem = null;
-    var tabList = this.data.WedgetsLongPressMoveData.tabList;
-    tabList.forEach(function (obj, i) {
-      if (y > obj.y && y < (obj.y + obj.width)) {
-        nowItem = i;
-      }
-    })
-    this._pageScroll(clientY);
+  /**
+   * 组件选中
+   */
+  initSortData(tabList: any) {
+    var height = 0;
+    var ls = [];
+    for (var i = 0; i < tabList.length; i++) {
+      var obj = tabList[i];
+      obj.y = height;
+      ls.push(obj);
+      height = height + obj.width + 20;
+    }
+    // console.log(ls)
     this.setData({
-      nowItem: nowItem
+      tabList: ls,
+      viewHeight: height
     })
-  }
-},
-moveEnd(e:any) {
-  var moveId = this.data.WedgetsLongPressMoveData.moveId;
-  var nowItem = this.data.WedgetsLongPressMoveData.nowItem;
-  var tabList = this.data.WedgetsLongPressMoveData.tabList;
-  if (nowItem != null) {
-    var tmp = tabList[moveId];
-    // tabList[moveId] = tabList[nowItem]
-    // tabList[nowItem] = tmp;
+  },
+  //计算位置
+  // moveStatus(e:any) {
 
-    tabList.splice(moveId, 1);
-    tabList.splice(nowItem, 0, tmp);
-  }
-  this.setData({
-    moveId: null,
-    nowItem: null,
-    enable:false
-  })
-  this.initSortData(tabList);
-},
-/** 页面滑动 */
-_pageScroll(clientY:any) {
-  if (clientY + this.data.windowHeight*0.2 >= this.data.windowHeight) {
-    // 下滑接近屏幕底部
-    // console.log("down",clientY + (this.data.windowHeight - clientY)*20)
-    wx.pageScrollTo({
-      scrollTop: clientY + (this.data.windowHeight - clientY)*20,
-      duration: 5
-    });
-  } else if (clientY - this.data.windowHeight*0.2 <= 0) {
-    // 上滑接近屏幕顶部
-    // console.log("up",clientY + (clientY - this.data.windowHeight*0.2)*20)
-    wx.pageScrollTo({
-      scrollTop: clientY + (clientY - this.data.windowHeight*0.2)*20,
-      duration: 5
+  //   if (e.type == 'touchmove' && this.data.WedgetsLongPressMoveData.enable) {
+  //     //最终坐标
+  //     // console.log(e)
+  //     var y = e.changedTouches[0].pageY;
+  //     var clientY = e.changedTouches[0].clientY;
+  //     var nowItem = null;
+  //     var tabList = this.data.WedgetsLongPressMoveData.tabList;
+  //     tabList.forEach(function (obj, i) {
+  //       if (y > obj.y && y < (obj.y + obj.width)) {
+  //         nowItem = i;
+  //       }
+  //     })
+  //     this._pageScroll(clientY);
+  //     this.setData({
+  //       nowItem: nowItem
+  //     })
+  //   }
+  // },
+  // moveEnd(e:any) {
+  //   var moveId = this.data.WedgetsLongPressMoveData.moveId;
+  //   var nowItem = this.data.WedgetsLongPressMoveData.nowItem;
+  //   var tabList = this.data.WedgetsLongPressMoveData.tabList;
+  //   if (nowItem != null) {
+  //     var tmp = tabList[moveId];
+  //     // tabList[moveId] = tabList[nowItem]
+  //     // tabList[nowItem] = tmp;
+
+  //     tabList.splice(moveId, 1);
+  //     tabList.splice(nowItem, 0, tmp);
+  //   }
+  //   this.setData({
+  //     moveId: null,
+  //     nowItem: null,
+  //     enable:false
+  //   })
+  //   this.initSortData(tabList);
+  // },
+  /** 页面滑动 */
+  _pageScroll(clientY: any) {
+    if (clientY + this.data.windowHeight * 0.2 >= this.data.windowHeight) {
+      // 下滑接近屏幕底部
+      // console.log("down",clientY + (this.data.windowHeight - clientY)*20)
+      wx.pageScrollTo({
+        scrollTop: clientY + (this.data.windowHeight - clientY) * 20,
+        duration: 5
+      });
+    } else if (clientY - this.data.windowHeight * 0.2 <= 0) {
+      // 上滑接近屏幕顶部
+      // console.log("up",clientY + (clientY - this.data.windowHeight*0.2)*20)
+      wx.pageScrollTo({
+        scrollTop: clientY + (clientY - this.data.windowHeight * 0.2) * 20,
+        duration: 5
+      })
+    }
+  },
+  onLongPress(e: any) {
+    console.log(e)
+    var moveId = e.currentTarget.dataset.moveid;
+    this.setData({
+      enable: true,
+      moveId: moveId
     })
-  }
-},
-onLongPress(e:any){
-  console.log(e)
-  var moveId = e.currentTarget.dataset.moveid;
-  this.setData({
-    enable:true,
-    moveId:moveId
-  })
-},
-/**
- * 组件选中
- */
+  },
+  /**
+   * 组件选中
+   */
 
-/*
- * 选择主页组件还是我组件页面设置
- */
-chooseSeetingWidget(){
-  /**
-   * 选择组件页面设置
+  /*
+   * 选择主页组件还是我组件页面设置
    */
-  var that=this
-  that.setData({
-    isChooseSeetingwidget:true
-  })
-},
-/**
- * 选择主页小组件
- */
-chooseIndexWidget(){
+  chooseSeetingWidget() {
+    /**
+     * 选择组件页面设置
+     */
+    var that = this
+    that.setData({
+      isChooseSeetingwidget: true
+    })
+  },
   /**
-   * 选择组件页面设置
+   * 选择主页小组件
    */
-  var that=this
-  that.setData({
-    isChooseSeetingwidget:false//选择主页组件设置
-  })
-  that.getNodeInfo("oldRemindBar")
-},
+  chooseIndexWidget() {
+    /**
+     * 选择组件页面设置
+     */
+    var that = this
+    that.setData({
+      isChooseSeetingwidget: false//选择主页组件设置
+    })
+    that.getNodeInfo("oldRemindBar")
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   /**
    * 吸顶
    */
-    // 获取页面节点信息的方法(nodeName 节点id名称)
-    getNodeInfo(nodeName:String) {
-      const query = this.createSelectorQuery();
-      query.select(`#${nodeName}`).boundingClientRect();
-      query.selectViewport().scrollOffset();
-      query.exec(res => {
-        this.setData({ scrollTop: res[0].height });
-      });
-    },
+  // 获取页面节点信息的方法(nodeName 节点id名称)
+  getNodeInfo(nodeName: String) {
+    const query = this.createSelectorQuery();
+    query.select(`#${nodeName}`).boundingClientRect();
+    query.selectViewport().scrollOffset();
+    query.exec(res => {
+      this.setData({ scrollTop: res[0].height });
+    });
+  },
   onLoad() {
-    
+
     // 获取屏幕高度
     // TODO: 迁移到app.js中因为已经多处调用
     var windowHeight = wx.getSystemInfoSync().windowHeight;
     this.setData({
       windowHeight: windowHeight
     })
-    var that=this
+    var that = this
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -246,8 +246,8 @@ chooseIndexWidget(){
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    var that=this
-    
+    var that = this
+
   },
   /**
    * 生命周期函数--监听页面隐藏
