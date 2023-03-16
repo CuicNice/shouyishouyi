98b563818029a41cda59f77a38853bb02701e6e7
‘cuicui’ <825115818@qq.com>
2023-03-17 01:06:45 +0800

    3.17 样式修改啊

--- miniprogram/pages/widgets/classSchedule/classScheduleComponent/classScheduleComponent.ts
+++ miniprogram/pages/widgets/classSchedule/classScheduleComponent/classScheduleComponent.ts
@@ -11,13 +11,32 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    dailySchedule:[],
+    time:[],
+    dayTime: [] as any,
   },
-
+  pageLifetimes: {
+    show: function() {
+      // 页面被展示
+      this.getdailySchedule();
+    },
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-
-  }
+    godailySchedule: function () { //进入日课表页面
+    },
+    // 获取缓存
+  getdailySchedule(){
+      var that = this;
+      var dailySchedule = wx.getStorageSync('widget-classSchedule').dailySchedule;
+      var time=wx.getStorageSync('widget-classSchedule').time;
+        that.setData({
+          dailySchedule: dailySchedule,
+          time:time 
+        });
+        console.log(dailySchedule);
+  },
+}
 })
